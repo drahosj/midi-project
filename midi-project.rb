7176de6e145789418043858c95b96f0715465204
Jake Drahos <drahos.jake@gmail.com>
2012-12-01 15:41:54 -0600

    Added shepard_on and shepard_off
    Turn on and off notes based on place in shepard tone scale

--- midi-project.rb
+++ midi-project.rb
@@ -28,9 +28,28 @@ class MidiAdaptor
   def note_off note, velocity
     @output.puts(0x80, note, velocity)
   end
-  def shepard_note note #Pass 0-23, ret [note, velocity, note', velocity']
+  def shepard_on note
+    tones = shepard_tone note
+    note_on tones[0], tones[1]
+    note_on tones[2], tones[3]
+  end
+  def shepard_off note
+    tones = shepard_tone note
+    note_off tones[0], tones[1]
+    note_off tones[2], tones[3]
+  end
+  def shepard_tone note 
     retval = []
     retval << 48 + note;
-    
-    velocity = (100 * Math::exp(-(((note - 12)**2)/72 - 12.00)))).to_i
+    retval << (100 * Math::exp(-(((note - 12)**2)/72.0))).to_i
+
+    if note < 12
+      retval << 60 + note;
+      retval << (100 * Math::exp(-((note**2)/72.0))).to_i
+    else
+      retval << 36 + note;
+      retval << (100 * Math::exp(-(((note - 24)**2)/72.0))).to_i
+    end
+    return retval
+  end
 end
