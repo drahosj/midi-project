454fc4547f0f96911eca2d5b7c487259b0e73a06
Jake Drahos <drahos.jake@gmail.com>
2012-12-03 15:42:04 -0600

    Set midi-project to execute a test when run

--- midi-project.rb
+++ midi-project.rb
@@ -47,16 +47,23 @@ class MidiAdaptor
     end
     return retval
   end
-end
 
-def test_adaptor
-  adaptor = MidiAdaptor.new
-  
-  4.times do
-    24.times do |n|
-      adaptor.shepard_on n
-      sleep 0.35
-      adaptor.shepard_off n
+  def test
+    4.times do
+      24.times do |n|
+        shepard_on n
+        sleep 0.35
+        shepard_off n
+      end
     end
   end
+end
+
+if __FILE__ == $0
+  adaptor = MidiAdaptor.new
+  at_exit do
+    puts "Shutting down midi adaptor..."
+    adaptor.shutdown
+  end
+  adaptor.test
 end
